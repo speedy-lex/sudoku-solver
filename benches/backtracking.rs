022e893107b424f3f77872840182ee38713f9a32
Speedy_Lex <78314533+speedy-lex@users.noreply.github.com>
2025-08-13 13:23:51 +0200

    add arto_inkala bench

--- benches/backtracking.rs
+++ benches/backtracking.rs
@@ -15,6 +15,10 @@ fn solve_ai_escargot() -> Board {
     backtracking::solve(Board::from_str("1----7-9--3--2---8--96--5----53--9---1--8---26----4---3------1--4------7--7---3--").unwrap())
 }
 #[divan::bench]
+fn solve_arto_inkala() -> Board {
+    backtracking::solve(Board::from_str("8----------36------7--9-2---5---7-------457-----1---3---1----68--85---1--9----4--").unwrap())
+}
+#[divan::bench]
 fn generate_map() -> Map {
     let b = Board::from_str("-8------9--12--3-87--------4------7-2-8--5------79-8----95-4----149-6------18--5-").unwrap();
     Map::generate(&b)
